a886ed8fd098da7f8923f125ab3204bed78aab1d
Vibodh <vibodhbhoure@gmail.com>
2022-08-01 22:08:06 +0530

    first-commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.use(express.static('public'));
+app.get("/",function(req, res){
+    res.sendFile(__dirname+"/index.html");
+});
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Listening at port 3000!");
+});
